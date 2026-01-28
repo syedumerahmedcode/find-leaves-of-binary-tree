a34a2b48138b56cc46d30360f289f1636eba5350
umer <syedumerahmedcode@gmail.com>
2026-01-28 08:40:21 +0100

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,55 @@
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+        
+        
+        
+        Example:
+        
+        Input: [1,2,3,4,5]
+        
+          1
+         / \
+        2   3
+        / \
+        4   5
+        
+        Output: [[4,5,3],[2],[1]]
+        
+        
+        Explanation:
+        
+        1. Removing the leaves [4,5,3] would result in this tree:
+        
+          1
+         /
+        2
+        
+        
+        2. Now removing the leaf [2] would result in this tree:
+        
+          1
+        
+        
+        3. Now removing the leaf [1] would result in the empty tree:
+        
+          []
+         */
+        // Creating a test binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.collectLeaves(root);
+
+        System.out.println("The leaf nodes collected for the binary tree constrcted above are: "+result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,52 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
+    public List<List<Integer>> collectLeaves(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        /**
+         * Iterate through the Binary tree and keep on collecting leaves until the 
+         * root itself is null
+         */
+        while (root != null) {
+            /**
+             * This provides us leaves, level by level
+             */
+            List<Integer> leaves = new ArrayList<>();
+            root = removeLeaves(root, leaves);
+            result.add(leaves);
+        }
+        return result;
+    }
+    
+    private TreeNode removeLeaves(TreeNode node, List<Integer> leaves) {
+        /**
+         * Base condition
+         */
+        if (node == null) {
+            return null;
+        }
+        /**
+         * Another base condition
+         */
+        if(node.left==null && node.right==null){
+            /**
+             * Add it to the list of leaves for this level
+             */
+            leaves.add(node.val);
+            /**
+             * remove the last leaf
+             */
+            return null;
+        }
+        node.left=removeLeaves(node.left, leaves);
+        node.right=removeLeaves(node.right, leaves);
+        /**
+         * return the updated tree
+         */
+        return node;
+    }
 }
